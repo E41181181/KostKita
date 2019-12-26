b7fa00a2b14e398943ebc3f1912d795994bac368
FitriaAziati999 <fitriaaziati999@gmail.com>
2019-12-26 11:40:18 +0700

    Delete hapus_wishlist.php

--- hapus_wishlist.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$ID_KOS= $_GET['id'];
-unset ($_SESSION["wishlist"][$ID_KOS]);
-echo "<script>alert('Kos Terhapus dari Daftar Wishlist Anda');</script>";
-echo "<script>location='wishlist.php';</script>";
-?>
